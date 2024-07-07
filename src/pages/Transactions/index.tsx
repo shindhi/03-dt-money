37447199458988c63f678800bd8c087ef2719c37
Shindhi <diogoshindhinaka@gmail.com>
2024-07-07 19:33:36 -0300

    adiciona tabela de transações

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,58 @@
+import { useEffect, useState } from "react";
+
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { PriceHighlight, TransactionContainer, TransactionTable } from "./styled";
+
+interface Transaction {
+  id: number
+  description: string
+  type: 'income' | 'outcome'
+  price: number
+  category: string
+  createdAt: string
+}
 
 export function Transactions() {
+  const [transactions, setTransactions] = useState<Transaction[]>([])
+  
+  async function loadTransaction() {
+    const response = await fetch('http://localhost:3333/transactions')
+    const data = await response.json()
+    
+    setTransactions(data)
+  }
+
+  useEffect(() => {
+    loadTransaction()
+  }, [])
+
   return (
     <div>
       <Header />
       <Summary />
+      
+      <TransactionContainer>
+
+        <TransactionTable>
+          <tbody>
+            {transactions.map(transaction => {
+              return (
+                <tr key={transaction.id}>
+                  <td width="50%">{transaction.description}</td>
+                  <td>
+                    <PriceHighlight variant={transaction.type}>
+                      {transaction.price}
+                    </PriceHighlight>
+                  </td>
+                  <td>{transaction.category}</td>
+                  <td>{transaction.createdAt}</td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </TransactionTable>
+      </TransactionContainer>
 
     </div>
   )
--- src/pages/Transactions/styled.ts
+++ src/pages/Transactions/styled.ts
@@ -1,2 +1,37 @@
 import styled from 'styled-components'
 
+export const TransactionContainer = styled.main`
+  width: 100%;
+  max-width: 112rem;
+  margin: 6.4rem auto 0;
+  padding: 0 2.4rem;
+`
+
+export const TransactionTable = styled.table`
+  width: 100%;
+  border-collapse: separate;
+  border-spacing: 0 0.8rem;
+  margin-top: 2.4rem;
+
+  td {
+    padding: 2rem 3.2rem;
+    background: ${({ theme }) => theme['gray-700']};
+    
+    &:first-child {
+      border-top-left-radius: 6px;
+      border-bottom-left-radius: 6px;
+    }
+
+    &:last-child {
+      border-top-right-radius: 6px;
+      border-bottom-right-radius: 6px;
+    }
+  }
+`
+
+interface PriceHighlightProps {
+  variant: 'income' | 'outcome'
+}
+export const PriceHighlight = styled.span<PriceHighlightProps>`
+  color: ${({ theme, variant }) => variant === 'income' ? theme['green-300'] : theme['red-300']};
+`
