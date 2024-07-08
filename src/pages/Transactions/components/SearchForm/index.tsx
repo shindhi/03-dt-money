9fbe18ea7b043f72e9946dcf861c296ccc3c6605
Shindhi <diogoshindhinaka@gmail.com>
2024-07-08 17:34:05 -0300

    adiciona funcionalidade de busca de transações

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -1,9 +1,11 @@
+import { useContext } from "react"
 import { MagnifyingGlass } from "phosphor-react"
 import { useForm } from "react-hook-form"
 import * as z from "zod"
 import { zodResolver } from "@hookform/resolvers/zod"
 
 import { SearchFormContainer } from './styled'
+import { TransactionsContext } from "../../../../contexts/TransactionsContext"
 
 const searchFormSchema = z.object({
   query: z.string()
@@ -12,13 +14,13 @@ const searchFormSchema = z.object({
 type searchFormInputs = z.infer<typeof searchFormSchema>
 
 export function SearchForm() {
+  const { fetchTransactions } = useContext(TransactionsContext)
   const { register, handleSubmit, formState: { isSubmitting } } = useForm<searchFormInputs>({
     resolver: zodResolver(searchFormSchema) 
   })
 
   async function handleSearchTransactions(data: searchFormInputs) {
-    await new Promise(resolver => setTimeout(resolver, 2000)) 
-    console.log(data)
+    await fetchTransactions(data.query)
   }
 
   return (
